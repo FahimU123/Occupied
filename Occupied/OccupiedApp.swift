1787e4442b5ff6b055a36ed01dfa4d75e134590b
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-11-14 09:19:21 -0500

    Initial Commit

---
+++ Occupied/OccupiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  OccupiedApp.swift
+//  Occupied
+//
+//  Created by Fahim Uddin on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct OccupiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
